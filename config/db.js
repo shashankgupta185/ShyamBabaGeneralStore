d53c6bc8efedbc9a8ce91658f5c81a1a81772de5
shashankgupta185 <105859419+shashankgupta185@users.noreply.github.com>
2026-02-23 00:05:08 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,21 +1,34 @@
+// const { Pool } = require("pg");
+// require("dotenv").config();
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   max: 10, // connectionLimit equivalent
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 2000,
+//   ssl: false, // set true if using cloud DB (Render, Neon, Supabase etc.)
+// });
+
+// console.log("Connecting to: - db.js:16", {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+// });
+// module.exports = pool;
+
+
+
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  max: 10, // connectionLimit equivalent
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-  ssl: false, // set true if using cloud DB (Render, Neon, Supabase etc.)
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-console.log("Connecting to: - db.js:16", {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-});
 module.exports = pool;
